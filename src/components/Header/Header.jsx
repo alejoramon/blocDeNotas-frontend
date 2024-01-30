70d3c4e92c7017c109ad2d1fcdea8fbed7299394
alejoramon <alejoramon@gmail.com>
2024-01-30 18:47:33 +0100

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import catImage from "./assets/appdenotas.png";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img id="img" src={catImage} alt="appdenotas" />
+    </header>
+  );
+};
+
+export default Header;
