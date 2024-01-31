41392d68100949ab0d8eba21aa5459ca7cc598b3
alejoramon <alejoramon@gmail.com>
2024-01-31 18:38:19 +0100

    Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Router } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Router>Hello World</Router>
+    </>
+  );
 }
 
 export default App;
